53d05777bac437a0884ec349a1347cb9cbdc6fc6
Lora <jonathan.lora.95@gmail.com>
2018-08-20 15:37:26 -0400

    fixed seed file and created script to run both schema and seed

--- app/db/schema.sql
+++ app/db/schema.sql
@@ -1,7 +1,5 @@
 DROP DATABASE IF EXISTS movie_dev;
 CREATE DATABASE movie_dev;
-DROP TABLE IF EXISTS reviews;
-DROP TABLE IF EXISTS users;
 
 \c movie_dev
 
@@ -9,14 +7,13 @@ DROP TABLE IF EXISTS reviews;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS movies;
 
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(15) NOT NULL UNIQUE,
-    pass_digest VARCHAR(25) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    icon VARCHAR(255),
-);
-
+-- CREATE TABLE users (
+--     id SERIAL PRIMARY KEY,
+--     username VARCHAR(15) NOT NULL UNIQUE,
+--     pass_digest VARCHAR(25) NOT NULL,
+--     email VARCHAR(255) NOT NULL UNIQUE,
+--     icon VARCHAR(255)
+-- );
 
 CREATE TABLE movies (
     id SERIAL PRIMARY KEY NOT NULL,
@@ -25,12 +22,12 @@ CREATE TABLE movies (
     genre VARCHAR(255) NOT NULL,
     year INTEGER NOT NULL,
     synopsis TEXT NOT NULL,
-    img VARCHAR(255) NOT NULL,
+    img VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE reviews (
     id SERIAL PRIMARY KEY,
-    reviews_desc VARCHAR(255),
-    users_id INTEGER REFERENCES USERS,
+    review_desc VARCHAR(255),
+    -- users_id INTEGER REFERENCES USERS,
     movie_id INTEGER REFERENCES MOVIES
-)
+);
