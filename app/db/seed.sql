588ff103e9bc734685e1cf60857928e9b2913e58
kennyzhan9 <kennyzhan9@gmail.com>
2018-08-20 15:01:49 -0400

    first dummy seed test

---
+++ app/db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO movies 
+(title, movie_cast, genre, year, synopsis, img) 
+VALUES 
+(
+    'Birdman',
+    'Michael Keaton, Emma Stone, Edward Norton, Naomi Watts, Zach Galifianakis',
+    'Comedy, Drama',
+    2014,
+    'A washed-up actor, who once played an iconic superhero, attempts to revive his career by writing and starring in his very own Broadway play.',
+    'https://www.imdb.com/title/tt2562232/mediaviewer/rm3926180864',
+);
