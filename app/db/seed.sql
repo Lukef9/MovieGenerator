24f65f375e597d2448c4ec442fb012cd824f9db2
Luke Fariselli <Luke.fariselli@gmail.com>
2018-08-20 15:02:50 -0400

    one movie

---
+++ app/db/seed.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO movies (title, movie_cast, genre, year, synopsis, img)
+VALUES (
+  'The Longest Yard', 
+  'Adam Sandler', 
+  'comedy', 
+  1995, 
+  'A prison football comedy', 
+  'http://www.gstatic.com/tv/thumb/movieposters/35823/p35823_p_v8_ae.jpg'
+  );
+
+  INSERT INTO reviews ( reviews_desc, movie_id)
+  VALUES (
+    'funny movie',
+    1
+  );
