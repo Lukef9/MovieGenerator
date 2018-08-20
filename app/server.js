b89d0b955e7ceab5f30b8b75fdf9c2ce7dd3f50c
Lora <jonathan.lora.95@gmail.com>
2018-08-20 18:20:26 -0400

    Installed and Initialized Cors

--- app/server.js
+++ app/server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const movieRouter = require('./routes/movieRouter');
+const reviewRouter = require('./routes/reviewRouter');
+
+const PORT = process.env.PORT || 3001;
 
 app.use(logger('dev'));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Handles http requests made from the Front-end server(React)
+app.use(cors());
+
+app.use('/movies', movieRouter);
+app.use('/reviews', reviewRouter);
+
 app.get('/', (req, res) => {
-    res.send('Your server test is 200 OK');
+  res.send('Your server test is 200 OK');
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}, in ${app.get('env')} mode.`);
+  console.log(`Listening on ${PORT}, in ${app.get('env')} mode.`);
 });
