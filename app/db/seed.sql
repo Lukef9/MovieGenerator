1cde363f946ff7ea914e296a9f23a1f5401231aa
kennyzhan9 <kennyzhan9@gmail.com>
2018-08-20 15:10:59 -0400

    added dummy review seed

--- app/db/seed.sql
+++ app/db/seed.sql
@@ -9,3 +9,11 @@ VALUES
     'A washed-up actor, who once played an iconic superhero, attempts to revive his career by writing and starring in his very own Broadway play.',
     'https://www.imdb.com/title/tt2562232/mediaviewer/rm3926180864',
 );
+
+INSERT INTO reviews 
+(review_desc, movie_id)
+VALUES 
+(
+    'This movie was hilarious!',
+    2,
+);
