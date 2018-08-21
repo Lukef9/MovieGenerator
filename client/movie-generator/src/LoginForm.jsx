ef684571ebe3f11625f3551b1b42b031ffa4ea85
kennyzhan9 <kennyzhan9@gmail.com>
2018-08-21 11:31:32 -0400

    finished LoginForm.jsx

---
+++ client/movie-generator/src/LoginForm.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            pwDigest: ''
+        }
+
+        this.handleUsername = this.handleChange.bind(this, 'userName');
+        this.handlePwDigest = this.handleChange.bind(this, 'pwDigest');
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(input, evt) {
+        this.setState({
+            [input]: evt.target.value,
+        });
+    }
+
+    handleSubmit(evt) {
+        evt.preventDefault();
+        alert('Welcome Back, ' + this.state.userName + '!');
+    }
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text" 
+                        name="userName"
+                        placeholder="Username" 
+                        value={this.state.userName} 
+                        onChange={this.handleUsername} 
+                    />
+                    <input 
+                        type="password" 
+                        name="pwDigest"
+                        placeholder="Password" 
+                        value={this.state.pwDigest} 
+                        onChange={this.handlePwDigest} 
+                    />
+                    <button type="submit">Sign In</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default LoginForm;
