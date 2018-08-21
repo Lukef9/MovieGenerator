6ea33a436106e0ab4d087714fdf7de87979111ce
Luke Fariselli <Luke.fariselli@gmail.com>
2018-08-21 18:54:22 -0400

    able to fetch all

--- client/movie-generator/src/services/api.js
+++ client/movie-generator/src/services/api.js
@@ -1,67 +1,51 @@
 const BE_URL = 'http://localhost:3001';
 
-module.exports = {
+export async function fetchMovies() {
+  try {
+    const movies = await fetch(`${BE_URL}/movies`);
+    return movies.json();
+  } catch (e) {
+    throw (e);
+  }
+}
 
-  // fetchMovies() {
-  //   return fetch(`${BE_URL}/movies`)
-  //     .then(resp => resp.json())
-  //     .catch((err) => {
-  //       throw Error(err);
-  //     });
-  // },
+export async function fetchReviews() {
+  try {
+    const reviews = await fetch(`${BE_URL}/reviews`);
+    return reviews.json();
+  } catch (e) {
+    throw (e);
+  }
+}
 
-  async fetchMovies() {
-    try {
-      const movies = await fetch(`${BE_URL}/movies`);
-      movies.json();
-      return movies;
-    } catch (e) {
-      throw (e);
-    }
-  },
+export async function saveReview(review) {
+  try {
+    const opts = {
+      method: 'POST',
+      body: JSON.stringify(review),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const reviewData = await fetch(`${BE_URL}/reviews`, opts);
+    return reviewData.json();
+  } catch (e) {
+    throw (e);
+  }
+}
 
-  async fetchReviews() {
-    try {
-      const reviews = await fetch(`${BE_URL}/reviews`);
-      reviews.json();
-      return reviews;
-    } catch (e) {
-      throw (e);
-    }
-  },
-
-  async saveReview(review) {
-    try {
-      const opts = {
-        method: 'POST',
-        body: JSON.stringify(review),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      const reviewData = await fetch(`${BE_URL}/reviews`, opts);
-      reviewData.json();
-      return reviewData;
-    } catch (e) {
-      throw (e);
-    }
-  },
-
-  async updateReview(review) {
-    try {
-      const opts = {
-        method: 'PUT',
-        body: JSON.stringify(review),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      const reviewData = await fetch(`${BE_URL}/reviews/${review.id}`, opts);
-      reviewData.json();
-      return reviewData;
-    } catch (e) {
-      throw (e);
-    }
-  },
-
-};
+export async function updateReview(review) {
+  try {
+    const opts = {
+      method: 'PUT',
+      body: JSON.stringify(review),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const reviewData = await fetch(`${BE_URL}/reviews/${review.id}`, opts);
+    return reviewData.json();
+  } catch (e) {
+    throw (e);
+  }
+}
