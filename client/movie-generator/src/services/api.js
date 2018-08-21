a22e296ffc6c909127cca3a08852acd5bbf88a86
Lora <jonathan.lora.95@gmail.com>
2018-08-21 11:15:51 -0400

    completed fetch functions to test CRUD on react, installed eslint as a devD and initialized

--- client/movie-generator/src/services/api.js
+++ client/movie-generator/src/services/api.js
@@ -1,15 +1,10 @@
-const axios = require('axios');
-const instance = axios.create({
-    baseURL: 'http://localhost:3001',
-    timeout: 1000,
-    headers: { 'Content-Type': 'application/json' },
-});
+const BE_URL = 'http://localhost:3001';
 
 module.exports = {
 
     async fetchMovies() {
         try {
-            const movies = await fetch(`${BE_URL}/movies`)
+            const movies = await fetch(`${BE_URL}/movies`);
             movies.json();
             return movies;
         } catch (e) {
@@ -19,7 +14,7 @@ module.exports = {
 
     async fetchReviews() {
         try {
-            const reviews = await fetch(`${BE_URL}/reviews`)
+            const reviews = await fetch(`${BE_URL}/reviews`);
             reviews.json();
             return reviews;
         } catch (e) {
@@ -27,11 +22,38 @@ module.exports = {
         }
     },
 
-    async saveReview() {
+    async saveReview(review) {
         try {
-
+            const opts = {
+                method: 'POST',
+                body: JSON.stringify(review),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            const reviewData = await fetch(`${BE_URL}/reviews`, opts);
+            reviewData.json();
+            return reviewData;
         } catch (e) {
-            throw (e)
+            throw (e);
         }
     },
+
+    async updateReview(review) {
+        try {
+            const opts = {
+                method: 'PUT',
+                body: JSON.stringify(review),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            };
+            const reviewData = await fetch(`${BE_URL}/reviews/${review.id}`, opts);
+            reviewData.json();
+            return reviewData;
+        } catch (e) {
+            throw (e);
+        }
+    },
+    
 };
