e2f1696b4d9e50efd17fd886011da40ff5a88231
Lora <jonathan.lora.95@gmail.com>
2018-08-21 22:21:41 -0400

    refactored json web token auth code, installed jws and removed sessions

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -1,12 +1,8 @@
 const authRouter = require('express').Router();
 const authController = require('../controllers/auth');
 
-authRouter.route('/login')
-  .get(/* show login form */)
-  .post(authController.login /* handle user profile *//* handle login error */);
+authRouter.route('/login').post(authController.login);
 
-authRouter.route('/register')
-  .get(/* show register form */)
-  .post(authController.register /* handle user profile *//* handle register error */);
+authRouter.route('/register').post(authController.register);
 
 module.exports = authRouter;
