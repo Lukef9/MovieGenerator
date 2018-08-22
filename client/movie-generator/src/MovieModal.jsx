a1439c3b038020c0a587997a20397f183c37f280
Tylaine Jeffrey <tylainej@gmail.com>
2018-08-22 16:08:30 -0400

    finished the movie modal

--- client/movie-generator/src/MovieModal.jsx
+++ client/movie-generator/src/MovieModal.jsx
@@ -9,25 +9,30 @@ class MovieModal extends Component{
     };
 }
 
-
-
-
     render ()   {
     return (
         <div id="modal" >
-                <div className="modal-background"></div>
-                    <div className="modal-card">
-                        <header className="modal-card-head">
-                <p className="modal-card-title">Movie Title</p>
-                <button className="closeModal" aria-label="close"></button>
+            <div type="button" className="closeModal">X</div>
+                    <div className="inner-modal">
+                        <header className="modal-header">
+                <p className="modal-title">Movie Title</p>
              </header>
-    <section className="modal-card-body">
-        Reviews...
+    <section className="modal-body">
+    <figure className="movie-image">
+    <img/>
+
+    </figure>
+    <div className="syn-rev-wrapper">
+    <article className="synopsis">Synopsis Lorem ipsum dolor, s
+    it amet consectetur adipisicing elit. Illum quibusdam magni 
+    odio pariatur culpa officiis voluptas. </article>
+    <review className="reviews">
+        Reviews and Ratings veniam maiores quisquam fuga 
+    dolorum ipsa, cumque, aliquid harum consequatur saepe. 
+    Ratione, nemo vitae!
+    </review>
+    </div>
     </section>
-    <footer className="modal-card-foot">
-      <button class="button is-success"></button>
-      <button class="button">Cancel</button>
-    </footer>
   </div>
 </div>
 );
