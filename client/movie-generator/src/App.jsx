30e29502a86fddd974b0c5531abf31df1fee63bf
Lora <jonathan.lora.95@gmail.com>
2018-08-22 12:14:46 -0400

    switch statement for currentView, and built functions forCRUD functionality for reviews

--- client/movie-generator/src/App.jsx
+++ client/movie-generator/src/App.jsx
@@ -3,61 +3,119 @@ import './App.css';
 import SearchForm from './SearchForm';
 // import LoginForm from './LoginForm';
 import Footer from './Footer';
-import { 
+import {
   fetchMovies,
   fetchReviews,
   saveReview,
   updateReview,
-  } from './services/api';
+} from './services/api';
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentView: 'Movie Index',
+      selectedMovie: '',
+      selectedReviews: [],
       movies: [],
-      reviews: []
-    }
+      reviews: [],
+    };
+    this.showMovie = this.showMovie.bind(this);
+    this.showReviews = this.showReviews.bind(this);
+    this.createReview = this.createReview.bind(this);
+    this.editReview = this.editReview.bind(this);
   }
 
   componentDidMount() {
     fetchMovies()
-      .then(movieData => this.setState({ movies: movieData.movies, }));
+      .then(movieData => this.setState({ movies: movieData.movies }));
 
     fetchReviews()
       .then(reviewData => this.setState({ reviews: reviewData.reviews }));
+  }
+
+  showMovie(movie) {
+    this.setState({
+      selectedMovie: movie,
+      currentView: '', /* show one page containing forms */
+    });
+  }
+
+  showReviews(reviews) {
+    this.setState({
+      selectedReviews: reviews,
+      currentView: '', /* show one page containing reviews for selected movie */
+    });
+  }
+
+  async createReview(review) {
+    const reviewData = await saveReview(review);
+    this.fetchReviews();
+    this.setState({
+      currentView: '', /* show one */
+      selectedReviews: reviewData.reviews,
+    });
+  }
+
+  async editReview(review) {
+    const reviewData = await updateReview(review);
+    this.fetchReviews();
+    this.setState({
+      currentView: '',
+      selectedReviews: reviewData.reviews,
+    });
+  }
+
+  renderCurrentView() {
+    const { currentView } = this.state;
+    const { movies, reviews, selectedMovie, selectedReviews } = this.state;
+
+    switch (currentView) {
+      case 'Movie Index':
+        return <MovieIndex movies={movies}/>;
+      case 'Show One':
+        return (<Movie 
+          selectedMovie={selectedMovie}
+          showMovie={this.showMovie}
+          selectedReviews={selectedReviews}
+          showReviews={this.showReviews}
+          reviews={reviews}
+          onCreate={this.createReview}
+          onUpdate={this.updateReview} />);
+    }
   }
 
   render() {
     return (
       <main className="App">
         <header className="App-header">
-        <div className="App-logo">Logo</div>
-        <div className="Flex-div">
-        <div className="Register">Register</div>
-        <div className="Login">Login</div>
-        </div>
+          <div className="App-logo">Logo</div>
+          <div className="Flex-div">
+            <div className="Register">Register</div>
+            <div className="Login">Login</div>
+          </div>
         </header>
         <div className="Search-bar">
-        <SearchForm/>
+          <SearchForm />
         </div>
         <section className="Main-section">
           <h2 className="Header-bar">Top 10 Movies</h2>
-            <div className="collected-divs">
+          <div className="collected-divs">
             <div className="Top-movies">
             This is you're selected movie
-            </div>
+              </div>
 
-            </div>
-            <h2 className="Top-selection">Top 10 Genre</h2>
-            <div className="more-collected-divs">
+          </div>
+          <h2 className="Top-selection">Top 10 Genre</h2>
+          <div className="more-collected-divs">
             <div className="Top-movies">
             This is you're selected movie
-            </div>
-            </div>
+              </div>
+          </div>
         </section>
         <footer className="Footer">
-        <Footer/>
+          <Footer />
 
         </footer>
       </main>
