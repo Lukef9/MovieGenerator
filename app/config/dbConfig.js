35e56a41a2066cd4c9066df34fed5ad21dd0e104
Luke Fariselli <Luke.fariselli@gmail.com>
2018-08-23 16:11:15 -0400

    setting up heroku

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,4 +1,6 @@
-module.exports = {
+
+
+module.exports = process.env.DATABASE_URL || {
   port: process.env.PORT || 5432,
   host: process.env.HOST || 'localhost',
   database: process.env.DATABASE || 'movie_dev',
