e111a49c320aebbe15d7fe773c3a2c38e0ae6b80
Luke Fariselli <Luke.fariselli@gmail.com>
2018-08-23 19:49:35 -0400

    something

--- client/movie-generator/src/Homepage.jsx
+++ client/movie-generator/src/Homepage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import MovieList from './MovieList';
 
-function Homepage({ movies, show, toggle }) {
+function Homepage({ movies, show, toggle, toShowOne }) {
   return (
     <section className="Main-section">
       <h2 className="Header-bar">Top 10 Movies</h2>
-      <MovieList movies={movies} show={show} toggle={toggle} />
+      <MovieList movies={movies} show={show} toggle={toggle} toShowOne={toShowOne} />
       
     </section>
   );
