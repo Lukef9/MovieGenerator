5af3347d69f8ddb3eac8e53284764de681b88180
Lora <jonathan.lora.95@gmail.com>
2018-08-24 10:03:15 -0400

    commented back in the controller middlewares for movie

--- app/controllers/movieController.js
+++ app/controllers/movieController.js
@@ -26,51 +26,51 @@ module.exports = {
     }
   },
   /* FOR FUTURE USE */
-  // async createMovie(req, res, next) {
-  //   try {
-  //     const {
-  //       title, movie_cast, genre, year, synopsis, img, movie_rating,
-  //     } = req.body;
-  //     const newMovie = await db.insertNewMovie({
-  //       title, movie_cast, genre, year, synopsis, img, movie_rating,
-  //     });
-  //     res.locals.data = newMovie;
-  //     next();
-  //   } catch (e) {
-  //     throw (e);
-  //   }
-  // },
+  async createMovie(req, res, next) {
+    try {
+      const {
+        title, movie_cast, genre, year, synopsis, img, movie_rating,
+      } = req.body;
+      const newMovie = await db.insertNewMovie({
+        title, movie_cast, genre, year, synopsis, img, movie_rating,
+      });
+      res.locals.data = newMovie;
+      next();
+    } catch (e) {
+      throw (e);
+    }
+  },
 
-  // async updateMovie(req, res, next) {
-  //   try {
-  //     const {
-  //       title, movie_cast, genre, year, synopsis, img, movie_rating,
-  //     } = req.body;
-  //     const modifiedMovie = {
-  //       movie_id: req.params.id,
-  //       title,
-  //       movie_cast,
-  //       genre,
-  //       year,
-  //       synopsis,
-  //       img,
-  //       movie_rating,
-  //     };
-  //     const updatedMovie = await db.updateThisMovie(modifiedMovie);
-  //     res.locals.data = updatedMovie;
-  //     next();
-  //   } catch (e) {
-  //     throw (e);
-  //   }
-  // },
+  async updateMovie(req, res, next) {
+    try {
+      const {
+        title, movie_cast, genre, year, synopsis, img, movie_rating,
+      } = req.body;
+      const modifiedMovie = {
+        movie_id: req.params.id,
+        title,
+        movie_cast,
+        genre,
+        year,
+        synopsis,
+        img,
+        movie_rating,
+      };
+      const updatedMovie = await db.updateThisMovie(modifiedMovie);
+      res.locals.data = updatedMovie;
+      next();
+    } catch (e) {
+      throw (e);
+    }
+  },
 
-  // async destroyMovie(req, res, next) {
-  //   try {
-  //     await db.deleteThisMovie(req.params.id);
-  //     next();
-  //   } catch (e) {
-  //     throw (e);
-  //   }
-  // },
+  async destroyMovie(req, res, next) {
+    try {
+      await db.deleteThisMovie(req.params.id);
+      next();
+    } catch (e) {
+      throw (e);
+    }
+  },
 
 };
